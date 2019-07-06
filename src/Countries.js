639d75ce4ea423347b9ee9f13e54edd603bfc893
Jay H.J Kwon <nicekhj@gmail.com>
2019-07-06 21:58:41 +1000

    pre loop rendering

--- src/Countries.js
+++ src/Countries.js
@@ -1,13 +1,7 @@
 import React from 'react'
 
-const Countries = ({ countryList }) => {
-    return (
-        <ul>
-            {countryList.map(({ id, name }) => 
-                <li key={id}>{name}</li>
-            )}
-        </ul>
-    )
+const Countries = () => {
+    return null;
 }
 
 export default Countries;
