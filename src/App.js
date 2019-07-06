d4d595d671a7f304b7250122fc1644c1f0d7de52
Jay H.J Kwon <nicekhj@gmail.com>
2019-07-07 01:51:42 +1000

    wip

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Countries from './Countries';
+import { orderBy } from 'lodash';
 
 const countryList = [
   { id: 1, name: 'Korea' },
   { id: 2, name: 'Australia' },
   { id: 3, name: 'China' }
 ]
+
+/*
+ - order by ascending 
+   orderBy(countryList, ['name'], ['asc'])
+ - order by descending
+   orderBy(countryList, ['name'], ['desc'])
+*/
 
 class App extends Component {
   render() {
