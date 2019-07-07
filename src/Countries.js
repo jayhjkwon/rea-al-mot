81a8cb9cef08ced1dbc8f6fb1df7eeb75446093a
Jay Hyojung Kwon <hyojung.kwon@ue.com.au>
2019-07-07 17:22:28 +1000

    pre graphql

--- src/Countries.js
+++ src/Countries.js
@@ -3,13 +3,9 @@ import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 
 const GET_COUNTRIES = gql`
-    query {
-        countries {
-            code
-            name
-            emoji
-        }
-    }
+    # TODO 
+    # 여기에 code, name, emoji 필드를 가져오는 GraphQL 쿼리를 삽입합니다. 
+    # https://countries.trevorblades.com/
 `;
 
 const Countries = ({ data: { countries, loading } }) => {
@@ -25,6 +21,10 @@ const Countries = ({ data: { countries, loading } }) => {
     )
 }
 
-const CountriesWithQuery = graphql(GET_COUNTRIES)(Countries);
+// TODO
+// graphql 함수를 사용해서 GraphQL 쿼리와 리액트 컴포넌트를 연결해보세요
+// Apollo Client 문서를 참조하세요
+// https://www.apollographql.com/docs/react/api/react-apollo/#graphqlquery-configcomponent
+const CountriesWithQuery = 
 
 export default CountriesWithQuery;
